fc8eab72421ce1089291a42c685591f9abda7acf
kishanchakraborty833@gmail.com <kishanchakraborty833@gmail.com>
2024-10-27 15:28:34 +0530

    tensor subtract operation and corresponding unit tests

--- Tests/test_tensor_sub.py
+++ Tests/test_tensor_sub.py
@@ -2,16 +2,17 @@ import unittest
 
 from AutoGrad.tensor import Tensor
 
+
 class TestTensorSum(unittest.TestCase):
     def test_simple_add(self):
         t1 = Tensor([1, 2, 3], requires_grad=True)
         t2 = Tensor([4, 5, 6], requires_grad=True)
 
-        t3 = t1-t2
-        t3.backward(Tensor([-1., -2., -3.]))
+        t3 = t1 - t2
+        t3.backward(Tensor([-1.0, -2.0, -3.0]))
 
-        assert t1.grad.data.tolist() == [-1., -2., -3.]
-        assert t2.grad.data.tolist() == [1., 2., 3.]
+        assert t1.grad.data.tolist() == [-1.0, -2.0, -3.0]
+        assert t2.grad.data.tolist() == [1.0, 2.0, 3.0]
 
     def test_broadcast_add(self):
         # What is broadcasting? A couple of things:
@@ -25,27 +26,28 @@ class TestTensorSum(unittest.TestCase):
         # I can expand it
         # t1 as (10, 5) t2 as (1, 5) is [[1, 2, 3, 4, 5]]
 
-        t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad = True)       # (2, 3)
-        t2 = Tensor([[7, 8, 9]], requires_grad = True)                  # (3,)
-        t3 = Tensor([[10, 11, 12]], requires_grad = True)               # (3,)
+        t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)  # (2, 3)
+        t2 = Tensor([[7, 8, 9]], requires_grad=True)  # (3,)
+        t3 = Tensor([[10, 11, 12]], requires_grad=True)  # (3,)
 
-        t4 = t1 - t2   # shape (2, 3)
+        t4 = t1 - t2  # shape (2, 3)
         t5 = t4 - t3
         t5.backward(Tensor([[1, 1, 1], [1, 1, 1]]))
 
         assert t1.grad.data.tolist() == [[1, 1, 1], [1, 1, 1]]
-        assert t2.grad.data.tolist() == [[2, 2, 2]]
-        assert t3.grad.data.tolist() == [[2, 2, 2]]
+        assert t2.grad.data.tolist() == [[-2, -2, -2]]
+        assert t3.grad.data.tolist() == [[-2, -2, -2]]
 
     def test_broadcast_add2(self):
-        t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad = True)    # (2, 3)
-        t2 = Tensor([[7, 8, 9]], requires_grad = True)               # (1, 3)
+        t1 = Tensor([[1, 2, 3], [4, 5, 6]], requires_grad=True)  # (2, 3)
+        t2 = Tensor([[7, 8, 9]], requires_grad=True)  # (1, 3)
 
-        t3 = t2 + t1
+        t3 = t2 - t1
         t3.backward(Tensor([[1, 1, 1], [1, 1, 1]]))
 
-        assert t1.grad.data.tolist() == [[1, 1, 1], [1, 1, 1]]
+        assert t1.grad.data.tolist() == [[-1, -1, -1], [-1, -1, -1]]
         assert t2.grad.data.tolist() == [[2, 2, 2]]
+
 
 if __name__ == "__main__":
     unittest.main()
